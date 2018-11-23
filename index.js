014542bf5e200b806369ecd47f60c84bb6c2d689
Fauzi <rizael.ichigo28@gmail.com>
2018-11-23 19:17:12 +0700

    Mengenal var,let dan const

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ var message = "Hello Coding";
 let msg1 = "Hallo";
 const msg2 = "Coding";
 
-console.log(msg2)
+if(true){
+    var message = 1;
+    let msg1 = 2;
+}
+
+console.log(message)
+console.log(msg1)
